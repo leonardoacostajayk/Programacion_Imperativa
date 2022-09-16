c4d596090c702a59ae190853eaa58a85c7f8834e
leonardoacostajayk <leonardoacostajayk@outlook.com>
2022-09-16 16:24:33 -0500

    bublesort ej

--- mesatrabajo20.js
+++ mesatrabajo20.js
@@ -6,22 +6,59 @@
 // que participaron 15 usuarios y suponer para cada uno, una cantidad de likes—.
 
 // Funcion que ordena un array de objetos  de forma Ascendente
-function ordenarArrayAsc(array,propiedad){
-
-    for(let i=0 ; i<array.length ; i++){
-
-        for(let j=0 ; j<array.length - 1 ; j++){
-    
-            if(array[j][propiedad] > array[j+1][propiedad]){
-    
-                let aux = array[j];
-                array[j] = array[j+1];
-                array[j+1] = aux;
-            }
-        }
+let arrayuserfoto=[
+    {
+        username: 'leonardo',
+        clikes:1000
+    },
+    {
+        username: 'mapaz',
+        clikes:10567
+    },
+    {
+        username: 'mati',
+        clikes:1976
+    },
+    {
+        username: 'yenvi',
+        clikes:50890
+    },
+    {
+        username: 'doroteo',
+        clikes:20
+    },
+    {
+        username: 'file',
+        clikes:2000
+    },
+    {
+        username: 'gato1',
+        clikes:56
+    },
+    {
+        username: 'gato2',
+        clikes:566
     }
-    return array;
-}
+
+]
+
+
+// function ordenarArrayAsc(array,propiedad){
+
+//     for(let i=0 ; i<array.length ; i++){
+
+//         for(let j=0 ; j<array.length - 1 ; j++){
+    
+//             if(array[j][propiedad] > array[j+1][propiedad]){
+    
+//                 let aux = array[j];
+//                 array[j] = array[j+1];
+//                 array[j+1] = aux;
+//             }
+//         }
+//     }
+//     return array;
+// }
 
 // Funcion que ordena un array de objetos  de forma Descendente
 function ordenararrayDesc(array,propiedad){
@@ -39,6 +76,10 @@ function ordenararrayDesc(array,propiedad){
     }
     return array;
 }
-function likesFotografia(array,ordenDesc,propiedad) 
+function likesFotografia(array,propiedad) 
 {
+    let arregloOrdenadoDesc= ordenararrayDesc(array,'clikes');
+
+   return arregloOrdenadoDesc[0];
 }
+console.log(likesFotografia(arrayuserfoto,'clikes'));
